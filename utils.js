815e099d0ed69e526f06722528a3afdac09a8984
Birthman <84112536+Birthman@users.noreply.github.com>
2025-06-14 22:30:40 -0400

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,1 +1,34 @@
+// utils.js
+
+/**
+ * Shuffles an array in place (Fisher-Yates algorithm)
+ * @param {Array} array
+ * @returns {Array}
+ */
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+/**
+ * Starts a countdown timer and calls callback when done
+ * @param {number} seconds
+ * @param {function} onTick
+ * @param {function} onComplete
+ */
+function startTimer(seconds, onTick, onComplete) {
+  let time = seconds;
+  onTick(time);
+  const interval = setInterval(() => {
+    time--;
+    onTick(time);
+    if (time <= 0) {
+      clearInterval(interval);
+      onComplete();
+    }
+  }, 1000);
+}
 
